0bb2accda883a13b4f32677354fe689da7c4072e
kclarke85 <kclarke85@hotmail.com>
2024-01-10 23:01:25 -0500

    Update pi_stream.py

--- pi_stream.py
+++ pi_stream.py
@@ -9,3 +9,10 @@ db = client["webapp_subscribe"]
 negative_collection = db['spoken_negative_words']
 positive_collection = db['all_words_phrases']
 
+# Set the page layout to have a centered title
+st.set_page_config(layout="wide")
+
+# Embed a logo on the left
+st.image('https://img1.wsimg.com/isteam/ip/e66af92a-07a8-4ac6-8d3f-a41caa301a88/blob-65affbe.png/:/rs=w:184,h:158,cg:true,m/cr=w:184,h:158/qt=q:95', use_column_width=100)
+
+
