ef59a553067e1feccd30a9e2ce13683077f46338
kclarke85 <kclarke85@hotmail.com>
2024-01-10 23:33:53 -0500

    Update pi_stream.py

--- pi_stream.py
+++ pi_stream.py
@@ -4,6 +4,7 @@ import plotly.express as px
 from pymongo import MongoClient
 from plotly.subplots import make_subplots
 import plotly.figure_factory as ff
+from streamlit.components.v1 import IFrame
 
 # Connect to MongoDB
 client = MongoClient("mongodb+srv://doadmin:NX09a6Z7m28K3d1E@Subc-36597421.mongo.ondigitalocean.com/webapp_subscribe?tls=true&authSource=admin&replicaSet=Subc")
@@ -18,6 +19,7 @@ st.set_page_config(layout="wide")
 st.image('https://img1.wsimg.com/isteam/ip/e66af92a-07a8-4ac6-8d3f-a41caa301a88/blob-65affbe.png/:/rs=w:184,h:158,cg:true,m/cr=w:184,h:158/qt=q:95', use_column_width=100)
 
 # Embed video from Dropbox
-st.write("Embedded Video from Dropbox:")
+st.write("RealTime Encounter:")
 video_url = "https://www.dropbox.com/scl/fi/3tmjbbkjdi920kw63fq2d/out.avi?rlkey=g2h7ly0v91hkraohltjk80fc2&dl=0/out.avi"
 st.video(video_url)
+st.components.v1.iframe(video_url, width=640, height=360)
