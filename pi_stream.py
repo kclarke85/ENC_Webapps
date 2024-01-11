e48b7f75e1697357a76f1e8659320465906165bf
kclarke85 <kclarke85@hotmail.com>
2024-01-10 23:47:06 -0500

    Update pi_stream.py

--- pi_stream.py
+++ pi_stream.py
@@ -4,7 +4,7 @@ import plotly.express as px
 from pymongo import MongoClient
 from plotly.subplots import make_subplots
 import plotly.figure_factory as ff
-from streamlit.components.v1 import IFrame
+from streamlit.components.v1 
 
 # Connect to MongoDB
 client = MongoClient("mongodb+srv://doadmin:NX09a6Z7m28K3d1E@Subc-36597421.mongo.ondigitalocean.com/webapp_subscribe?tls=true&authSource=admin&replicaSet=Subc")
