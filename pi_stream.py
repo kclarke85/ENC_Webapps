b4f6a1e04bc1a4051b95d483768737096225384f
kclarke85 <kclarke85@hotmail.com>
2024-01-14 12:22:19 -0500

    Update pi_stream.py

--- pi_stream.py
+++ pi_stream.py
@@ -9,3 +9,42 @@ DROPBOX_ACCESS_TOKEN = 'sl.BtomJzI-qj3UJftIPlfQM46rRSPgGmgFQuGmXarIvKa8bcyu5edkl
 
 # Dropbox video path
 DROPBOX_VIDEO_PATH = '/remote/mete-data/out.mp4'
+
+# Set the page layout to have a centered title
+st.set_page_config(layout="wide")
+
+# Embed a logo on the left
+st.image('https://img1.wsimg.com/isteam/ip/e66af92a-07a8-4ac6-8d3f-a41caa301a88/blob-65affbe.png/:/rs=w:184,h:158,cg:true,m/cr=w:184,h:158/qt=q:95', use_column_width=100)
+
+# Create the horizontal banner using HTML and CSS
+st.write(
+    """
+    <style>
+        .banner {
+            background-image: url('https://media.istockphoto.com/id/1160249819/photo/busy-city-intersection-with-technology-theme.jpg?s=612x612&w=0&k=20&c=FxT76zRLGmAmdlJsDqiSKiaGRdarQGt1JuBatq6l7N0=');
+            background-size: cover;
+            height: 100px;
+            width: 100%;
+        }
+    </style>
+    <div class="banner"></div>
+    """,
+    unsafe_allow_html=True
+)
+
+# Add custom CSS to change the button color to blue
+st.markdown(
+    """
+    <style>
+    .stButton > button {
+        background-color: #0074E4;
+        color: white;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Create additional information fields
+st.write("  ")
+col3, col4 = st.columns(2)
