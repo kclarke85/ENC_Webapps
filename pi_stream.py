82ae7104533f651b182e30fedb73c57eb95c50af
kclarke85 <kclarke85@hotmail.com>
2024-01-13 23:19:47 -0500

    Update pi_stream.py

--- pi_stream.py
+++ pi_stream.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 DROPBOX_ACCESS_TOKEN = 'sl.BtomJzI-qj3UJftIPlfQM46rRSPgGmgFQuGmXarIvKa8bcyu5edklJjNlw4itAGsf9_LfS2Paua_Nw07cYfN01bGSVNJj6SptoSpwEpt-aBwtIMeHttQXCH8pNSA91UyiOGjocrxgAk_e5a0_opmp9Y'
 
 # Dropbox video path
-DROPBOX_VIDEO_PATH = '/remote/out.mp4'
+DROPBOX_VIDEO_PATH = '/remote/mete-data/out.mp4'
 
 # Fetch video from Dropbox
 response = requests.post(
