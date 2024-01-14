3223283b43fd1a492e5bae9cf0d18ad218d78ad1
kclarke85 <kclarke85@hotmail.com>
2024-01-13 20:03:15 -0500

    Update pi_stream.py

--- pi_stream.py
+++ pi_stream.py
@@ -5,7 +5,6 @@ from pymongo import MongoClient
 from plotly.subplots import make_subplots
 import plotly.figure_factory as ff
 
-
 # Connect to MongoDB
 client = MongoClient("mongodb+srv://doadmin:NX09a6Z7m28K3d1E@Subc-36597421.mongo.ondigitalocean.com/webapp_subscribe?tls=true&authSource=admin&replicaSet=Subc")
 db = client["webapp_subscribe"]
@@ -17,3 +16,14 @@ st.set_page_config(layout="wide")
 
 # Embed a logo on the left
 st.image('https://img1.wsimg.com/isteam/ip/e66af92a-07a8-4ac6-8d3f-a41caa301a88/blob-65affbe.png/:/rs=w:184,h:158,cg:true,m/cr=w:184,h:158/qt=q:95', use_column_width=100)
+st.image('https://img1.wsimg.com/isteam/ip/e66af92a-07a8-4ac6-8d3f-a41caa301a88/blob-65affbe.png/:/rs=w:184,h:158,cg:true,m/cr=w:184,h:158/qt=q:95', use_column_width=100)
+
+# Code B
+st.video(video_stream)
+
+try:
+    # Your exception handling code
+    pass
+
+except Exception as e:
+    print(f"Error: {e}")
